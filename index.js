bfd379fa1c1d5a82343edd3089f010f7cac70b72
Tyler Hall <thall2980@gmail.com>
2022-08-30 17:12:47 -0400

    commit tree 94d4483af0d9773e2d56d296529ad4fa02a4b21b

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+
+//returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+//scuberDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multi) {
+    return function(fare) {
+        return multi * fare
+    }
+}
+
+const fareDoubler = function(createFareMultiplier) {
+    return createFareMultiplier * 2
+}
+
+const fareTripler = function(createFareMultiplier) {
+    return createFareMultiplier * 3
+}
+
+const selectDifferentDrivers = function(drivers, selection) {
+    if (selection === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(drivers)
+    } else if (selection === returnLastTwoDrivers)
+        return returnLastTwoDrivers(drivers)
+
+}
